c2ef1ea8747ce9eca53e84ab68e35fa7b8a396ee
chris <christianwilliamsjire@gmail.com>
2019-10-11 00:32:35 +0300

    this commit will add tags and post in models.py

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Post, tags
+
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post,PostAdmin)
+admin.site.register(tags)
--- gram/models.py
+++ gram/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
 
 # Create your models here.
 class Profile(models.Model):
@@ -17,3 +18,38 @@ class Profile(models.Model):
         
     class Meta:
         ordering = ['user']
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+class tags(models.Model):
+    name = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        
+class Post(models.Model):
+    post_image = models.ImageField(upload_to= 'images')
+    post_name = models.CharField(max_length=20)
+    post_description = models.TextField()
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add= True)
+    
+    def save_post(self):
+        self.save()
+        
+    def delete_post(self):
+        self.delete()
+        
+    @classmethod
+    def get_post(cls):
+        new = cls.objects.all()
+        return new
+    
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
