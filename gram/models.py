7678f4f3814723723f3e526412e1c99da2f9177f
chris <christianwilliamsjire@gmail.com>
2019-10-10 23:31:05 +0300

    this commit will add welcome function to the views

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save_profile(self):
+        self.save
+        
+    class Meta:
+        ordering = ['user']
