964d81ae0d8e594203a49e50df19299402750570
chris <christianwilliamsjire@gmail.com>
2019-10-11 12:30:48 +0300

    this commit will allows NewsletterRecipient function to send email to subsubscribers

--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 import datetime as dt
-from .models import Post, Profile
-from django.http import HttpResponse
+from .models import Post, Profile, NewsletterRecipients
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .email import send_welcome_email
 from .forms import NewsLetterForm
 
@@ -13,7 +13,13 @@ def welcome(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsletterRecipients(name=name, email=email)
+            recipient.save()
+            send_welcome_email(name, email)
+            
+            HttpResponseRedirect('welcome')
     else:
         form = NewsLetterForm()
     
