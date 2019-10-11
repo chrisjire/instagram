d0376b53c7794e2da465b850dd183817615f531d
chris <christianwilliamsjire@gmail.com>
2019-10-11 11:22:36 +0300

    this commit will add the send email function on email.py and add the email html and txt files

--- gram/views.py
+++ gram/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 import datetime as dt
 from .models import Post, Profile
+from django.http import HttpResponse
+from .email import send_welcome_email
 
 # Create your views here.
 def welcome(request):
     new = Post.get_post()
     prof = Profile.get_profile()
     return render (request, 'index.html', {"new":new}, {"prof":prof})
+
 
