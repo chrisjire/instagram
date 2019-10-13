537844322326c54514309115e8e13ff7f355074f
chris <christianwilliamsjire@gmail.com>
2019-10-13 19:50:23 +0300

    this commit will add ProfileForm to forms.py file

--- gram/forms.py
+++ gram/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from .models import Profile
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+    
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
